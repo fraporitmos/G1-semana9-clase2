24997358e4ea582e4e2b4e6f887ffaa2b5f6382f
Fran Espino <fraporitmos>
2024-10-30 21:45:10 -0500

    Challengue

---
+++ practica.js
@@ -0,0 +1,29 @@
+/* 
+Titulo: Maquina dispensadora
+Descripción: 
+Se requiere escribir un programa que tenga  las opciones
+con los siguientes productos:
+
+1.Gaseosa : S/2.00
+2.Papitas: S/3.00
+3.Galleta: S/1.00
+4.Agua Mineral: S/4.00
+
+El usuario debera ingresar una opcion y luego deberá ingresar la moneda,
+solo se admite un valor maximo de moneda de S./5.00.
+
+El programa deberá retornar el vuelto y escribir en pantalla el producto 
+que acaba de comprar. 
+
+El programa debe reconocer si el monto de la moneda ingresada es menor al precio del producto elegido debera escribir en pantalla que el saldo es insuficiente.
+*/
+
+prompt(`
+==========================
+1.Gaseosa: 🥤  S/2.00 
+2.Papitas: 🍟 S/3.00
+3.Galleta: 🍪 S/1.00
+4.Agua Mineral: 🍶 S/4.00
+==========================
+Ingresa la opcion:
+`)
